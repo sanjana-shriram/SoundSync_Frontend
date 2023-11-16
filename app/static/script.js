d4b0e1f14e8f1ab8e7bcc9b9f870c6a67baea9cc
Sanjana Shriram <sanjanashriram@sanjanas-air-5.wifi.local.cmu.edu>
2023-11-16 15:13:06 -0500

    page animation only one page tho

---
+++ app/static/script.js
@@ -0,0 +1,12 @@
+gsap.from('.page', {duration: 1, x: '25%'} )
+
+
+
+
+
+// CSSPlugin.defaultTransformPerspective = 800;
+
+// var tl = new TimelineMax({repeat:2, yoyo:false});
+// tl.to(".page", 4, {rotationY:-180, transformOrigin:"50% 0%", ease:Linear.easeNone})
+//   ///hide the front page at -90 degrees
+//   .set(".page .front", {autoAlpha:0}, "-=2")
