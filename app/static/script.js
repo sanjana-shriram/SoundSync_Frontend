7bf78707a1bf34a8c28370cba6d3be1c946f940b
Sanjana Shriram <sanjanashriram@sanjanas-air-5.wifi.local.cmu.edu>
2023-11-17 19:56:02 -0500

    EOD friday code

--- app/static/script.js
+++ app/static/script.js
@@ -1,40 +1,24 @@
-// gsap.from('.page', {duration: 1, x: '25%'} )
+gsap.from('.page', {duration: 1, x: '25%'} )
 
 // gsap.from('.cursor', {duration: 2, x: '-10000%', ease: 'Linear.easeNone',} )
-const deviceWidth = window.innerWidth;
 
-// Assuming this.cursor is your cursor element and deviceWidth is the width of your device
+// Citation https://gsap.com/community/forums/topic/20383-consistent-speed-with-various-distance/
+
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Your GSAP animation code here
-
     const cursor = document.querySelector('.cursor');
-    // const travelRange = 20;
     this.tl = new TimelineLite({
       onComplete: function() {
-        // this.delay(2).restart(true);
         this.restart(true);
-
       },
     });
-    
     this.tl.fromTo(cursor, 6, {
       x: -100,
       ease: 'Linear.easeInOut',
     }, {
       x: 370,
-
       ease: 'Linear.easeInOut',
     });
-    
     this.tl.play();
-
   });
   
-
-
-// CSSPlugin.defaultTransformPerspective = 800;
-
-// var tl = new TimelineMax({repeat:2, yoyo:false});
-// tl.to(".page", 4, {rotationY:-180, transformOrigin:"50% 0%", ease:Linear.easeNone})
-//   ///hide the front page at -90 degrees
-//   .set(".page .front", {autoAlpha:0}, "-=2")
